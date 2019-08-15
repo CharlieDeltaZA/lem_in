c579fd08ceb049df7121bfaa4c9eb9f62f41d623
Calvin Diogo <calvin@diogo.co.za>
2019-08-15 16:51:15 +0200

    Update lem_in.c

--- srcs/lem_in.c
+++ srcs/lem_in.c
@@ -6,7 +6,7 @@
 /*   By: cdiogo <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/08/13 16:19:08 by cdiogo            #+#    #+#             */
-/*   Updated: 2019/08/15 14:36:55 by cdiogo           ###   ########.fr       */
+/*   Updated: 2019/08/15 16:51:10 by cdiogo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,5 +15,13 @@
 //TODO :)
 int		main(int ac, char **av)
 {
+	// lots of error checking
+	// input, room validation, link validation
+	// ignore # comments, accept ## commands (valid ones, so start and end)
+	// need to have a func that checks if a room is occupied.
+	// init the stack of rooms - need a struct
+	// gnl to read input from a file?
+	// must display input, empty line, then moves
+	// ^^ Input = num ants, ##commands, rooms, links
 	return (0);
 }
