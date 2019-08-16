357ad3a98535bdc19deb38c7a6f44def5c2e12c8
Calvin Diogo <calvin@diogo.co.za>
2019-08-16 15:22:39 +0200

    Adjust comment

--- srcs/lem_in.c
+++ srcs/lem_in.c
@@ -6,7 +6,7 @@
 /*   By: cdiogo <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/08/13 16:19:08 by cdiogo            #+#    #+#             */
-/*   Updated: 2019/08/15 16:51:10 by cdiogo           ###   ########.fr       */
+/*   Updated: 2019/08/16 14:44:24 by cdiogo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@
 int		main(int ac, char **av)
 {
 	// lots of error checking
-	// input, room validation, link validation
+	// input, room validation (Names won't start with '#' or 'L'), link validation
 	// ignore # comments, accept ## commands (valid ones, so start and end)
 	// need to have a func that checks if a room is occupied.
 	// init the stack of rooms - need a struct
