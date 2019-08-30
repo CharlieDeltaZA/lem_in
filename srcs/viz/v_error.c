19fc3a4c82ed3251834aca746513b5923fed5aab
Calvin Diogo <calvin@diogo.co.za>
2019-08-30 16:36:53 +0200

    Some form of a basic main

--- srcs/viz/v_error.c
+++ srcs/viz/v_error.c
@@ -6,7 +6,7 @@
 /*   By: cdiogo <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/08/29 20:52:14 by cdiogo            #+#    #+#             */
-/*   Updated: 2019/08/29 21:02:37 by cdiogo           ###   ########.fr       */
+/*   Updated: 2019/08/30 16:22:46 by cdiogo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,6 +30,6 @@ void	viz_error_free(t_viz *viz, const char *error)
 {
 	if (viz)
 		viz_free(&viz);
-	ft_putendl_col_fd(RED, error, 2);
+	ft_putendl_col_fd(RED, (char*)error, 2);
 	exit(1);
 }
--- srcs/viz/v_main.c
+++ srcs/viz/v_main.c
@@ -6,7 +6,7 @@
 /*   By: cdiogo <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/08/29 09:10:12 by cdiogo            #+#    #+#             */
-/*   Updated: 2019/08/29 09:10:59 by cdiogo           ###   ########.fr       */
+/*   Updated: 2019/08/30 16:36:37 by cdiogo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,22 @@
 
 int		main(void)
 {
+	t_viz	*visualizer;
 	//TODO
+	//read in from GNL (read until empty line, then set a bool to false to change the struct being written to?)
+	read_info();
+	//init the viz struct
+	visualizer = init_viz();
+	//allocate/populate to the viz struct
+	populate_viz(visualizer);
+	//draw links
+	draw_links();
+	//draw rooms
+	draw_rooms();
+	//draw ants
+	draw_ants();
+	//event loop (space to move through the turns)
+	event_loop(visualizer);
+	//free & close upon completion
+	viz_free(&visualizer);
 }
