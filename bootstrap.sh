748816dcd01548585c9adceb45f88b1edfd681fc
Calvin Diogo <calvin@diogo.co.za>
2019-09-19 12:24:07 +0200

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 
 if [[ $(command -v brew) == "" ]]; then
+	echo "--------------------------------------------------"
+	echo "You will need to close this terminal and restart it once Brew is installed."
+	echo "Once terminal is restarted, run this script again!"
+	echo "--------------------------------------------------"
     echo "Installing Homebrew"
     sh -c "$(curl -fsSL https://raw.githubusercontent.com/Tolsadus/42homebrewfix/master/install.sh)"
 else
